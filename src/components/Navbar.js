d995d2bf0441b167f6a08d3bb86a7965c56421b9
snivJL <julien.lejay@gmail.com>
2021-01-15 13:32:26 +0700

    navbar_v1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navbar.css";
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
-      <Link to="/">Home / Top rated</Link>
-      <Link to="/movies/upcoming">Upcoming</Link>
+      <div class="page">
+        <nav class="page__menu menu">
+          <ul class="menu__list r-list">
+            <li class="menu__group">
+              <Link to="/">
+                <a href="#0" class="menu__link r-link text-underlined">
+                  Top rated
+                </a>
+              </Link>
+            </li>
+            <li class="menu__group">
+              <Link to="/movies/upcoming">
+                <a href="#0" class="menu__link r-link text-underlined">
+                  Upcoming
+                </a>
+              </Link>
+            </li>
+            <li class="menu__group">
+              <a href="#0" class="menu__link r-link text-underlined">
+                Option #3
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
