bc354dcb5357e90b111322cfa6d7fb46938a3852
snivJL <julien.lejay@gmail.com>
2021-01-15 14:47:20 +0700

    add icons font awesome

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./moviecard.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faShareAlt } from "@fortawesome/free-solid-svg-icons";
 
 const MovieCard = ({ card }) => {
+  const url =
+    "https://fsmedia.imgix.net/cd/c9/5e/ba/4817/4d9a/93f0/c776ec32ecbc/lara-crofts-neck-looks-unnatural-in-the-new-poster-for-tomb-raider.png";
   return (
     <>
       <div class="movie_card" id="tomb">
@@ -9,35 +13,38 @@ const MovieCard = ({ card }) => {
           <div class="movie_header">
             <img
               className="locandina"
-              src="https://mr.comingsoon.it/imgdb/locandine/235x336/53750.jpg"
+              src={`https://image.tmdb.org/t/p/w500/${card.poster_path}`}
+              alt="poster"
             />
-            <h1>Tomb Raider</h1>
-            <h4>2018, Roar Uthaug</h4>
-            <span class="minutes">125 min</span>
+            <h1>{card.title}</h1>
+            <h4>{card.release_date}</h4>
+            <span class="minutes">{`${card.vote_average} (${card.vote_count} votes)`}</span>
             <p class="type">Action, Fantasy</p>
           </div>
           <div class="movie_desc">
             <p class="text">
-              Lara Croft, the fiercely independent daughter of a missing
-              adventurer, must push herself beyond her limits when she finds
-              herself on the island where her father disappeared.
+              {card.overview.length > 200
+                ? `${card.overview.slice(0, 200)}...`
+                : card.overview}
             </p>
           </div>
           <div class="movie_social">
             <ul>
               <li>
-                <i class="material-icons">share</i>
+                <FontAwesomeIcon style={{ color: "red" }} icon={faHeart} />
               </li>
               <li>
-                <i class="material-icons"></i>
-              </li>
-              <li>
-                <i class="material-icons">chat_bubble</i>
+                <FontAwesomeIcon style={{ color: "white" }} icon={faShareAlt} />
               </li>
             </ul>
           </div>
         </div>
-        <div class="blur_back tomb_back"></div>
+        <div
+          class="blur_back"
+          style={{
+            background: `url(https://image.tmdb.org/t/p/original/${card.backdrop_path})`,
+          }}
+        ></div>
       </div>
     </>
   );
